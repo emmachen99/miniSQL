b82dd384deceb1e8bef955dc3f9348760f3d55a2
Buckfry <Buckfryspj@gmail.com>
2014-11-02 11:18:39 +0800

    Bnode

---
+++ Bnode.cpp
@@ -0,0 +1,18 @@
+/*
+ * Bnode.cpp
+ *
+ *  Created on: 2014Äê11ÔÂ2ÈÕ
+ *      Author: Administrator
+ */
+
+#include "E:Bnode.h"
+
+Bnode::Bnode() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Bnode::~Bnode() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Bnode.h
@@ -0,0 +1,17 @@
+/*
+ * Bnode.h
+ *
+ *  Created on: 2014Äê11ÔÂ2ÈÕ
+ *      Author: Administrator
+ */
+
+#ifndef BNODE_H_
+#define BNODE_H_
+
+class Bnode {
+public:
+	Bnode();
+	virtual ~Bnode();
+};
+
+#endif /* BNODE_H_ */
